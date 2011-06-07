15b35ee2c2ab1164efc4a252761e4e6bab2f418f
zonedabone <zonedabone@gmail.com>
2011-06-07 15:10:17 -0700

    Changes Pt. 1

--- Ne0nx3r0/QuantumCircuits/QuantumCircuits.java
+++ Ne0nx3r0/QuantumCircuits/QuantumCircuits.java
@@ -6,12 +6,17 @@ import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.PluginManager;
+import com.nijiko.permissions.PermissionHandler;
+import com.nijikokun.bukkit.Permissions.Permissions;
+import org.bukkit.plugin.Plugin;
 
 public class QuantumCircuits extends JavaPlugin{
     private final QuantumCircuitsBlockListener blockListener = new QuantumCircuitsBlockListener(this);
     private final QuantumCircuitsPlayerListener playerListener = new QuantumCircuitsPlayerListener(this);
     public int MAX_LAG_TIME = 300;
-
+    public boolean USE_PERMISSIONS = false;
+    public static PermissionHandler permissionHandler;
+    
     public void onDisable(){
         System.out.println("[Quantum] Quantum Circuits Disabled");
     }
@@ -20,6 +25,7 @@ public class QuantumCircuits extends JavaPlugin{
         Configuration config = getConfiguration();
 
         int iMaxLagTime = config.getInt("maxlagtime",-1);
+        boolean usePermissions = config.getBoolean("usepermissions",false);
 
         //set default values if necessary
         if(iMaxLagTime == -1){
@@ -28,14 +34,40 @@ public class QuantumCircuits extends JavaPlugin{
             iMaxLagTime = 300;
             config.save();
         }
+        
+        if(!usePermissions){
+        	config.setProperty("usepermissions", false);
+        	config.save();
+        }
 
         MAX_LAG_TIME = iMaxLagTime;
+        USE_PERMISSIONS = usePermissions;
+        
+        if(USE_PERMISSIONS){
+        	setupPermissions();
+        }
 
         PluginManager pm = getServer().getPluginManager();
         pm.registerEvent(Event.Type.REDSTONE_CHANGE, blockListener, Priority.Normal, this);
         pm.registerEvent(Event.Type.PLAYER_INTERACT, playerListener, Priority.Normal, this);
 
         PluginDescriptionFile pdfFile = this.getDescription();
-        System.out.println("[Quantum] "+pdfFile.getName() + " version " + pdfFile.getVersion() + " ENABLED" );
+        if(!USE_PERMISSIONS){
+        	System.out.println("[Quantum] "+pdfFile.getName() + " version " + pdfFile.getVersion() + " ENABLED" );
+        }else{
+        	System.out.println("[Quantum] "+pdfFile.getName() + " version " + pdfFile.getVersion() + " ENABLED WITH PERMISSIONS" );
+        }
+    }
+    
+    private void setupPermissions() {
+        Plugin permissionsPlugin = this.getServer().getPluginManager().getPlugin("Permissions");
+
+        if (permissionHandler == null) {
+            if (permissionsPlugin != null) {
+                permissionHandler = ((Permissions) permissionsPlugin).getHandler();
+            } else {
+                System.out.println("Permission system not detected, defaulting to OP");
+            }
+        }
     }
 }
